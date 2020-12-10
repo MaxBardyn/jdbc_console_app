93f337c5ed68fdd7a02c772891b17cfb813a5162
MaxBardyn <maxbardyn@gmail.com>
2020-12-10 14:36:37 +0200

    add connection to db

--- src/main/java/lab4/DatabaseConnector.java
+++ src/main/java/lab4/DatabaseConnector.java
@@ -7,7 +7,7 @@ public class DatabaseConnector {
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/bardyn_lab3?useUnicode=true&serverTimezone=UTC";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Max20bardyn@";
+    private static final String PASSWORD = "root@";
 
     private static Connection databaseConnection;
 
